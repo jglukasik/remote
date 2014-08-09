11cc9eb1f3b5e2c9c1d7b7690ceaaba55262ea6a
root <root@d1stkfactory>
2014-08-09 09:09:38 +0000

    Fixed the input logic so any input can be accessed from any other input

--- cgi-bin/remote.pl
+++ cgi-bin/remote.pl
@@ -19,60 +19,50 @@ my %buttons = %apex_codes::buttons;
 
 my $q = CGI->new;
 my $action = $q->param('action');
-my $clicks = $q->param('clicks');
+my $count= $q->param('count');
 
 my $source_ip = $ENV{HTTP_X_FORWARDED_FOR};
 my $remote_ip = $ENV{REMOTE_ADDR};
 
-my $ua = LWP::UserAgent->new;
-my $function = "input";
-
 if ($remote_ip eq $home_ip || $source_ip eq $home_ip){
-  &take_action($action, $clicks);
+	&take_action($action, $count);
 }
 
-print "Content-type:text/html\n\n";
-print $button;
-print "<p>";
-print $clicks;
-print "<p>";
-print "Source ip:  ".$source_ip;
-print "<p>";
-print "Remote ip:  ".$remote_ip;
-
 sub take_action {
-  my $vol_delay = 300;
-  my $input_delay = 600;
-  my $url = "https://api.spark.io/v1/devices/$id/$function";
-
-  my %actions = (
-   "power" => ("power");
-   "tv" => ("tv");
-   "chromecast" => ("tv", "hdmi");
-   "laptop" => ("tv", "hdmi", "hdmi");
-   "xbox" => ("tv", "hdmi", "hdmi", "hdmi");
-   "volup" => ("volup");
-   "voldown" => ("voldown");
-   "mute" => ("mute");
-  }
-
-  my $action = $_;
-
-  if ($action =~ /^vol/){
-    my $clicks = $_;
-    my $code = $buttons{$actions};
-
-    for my $i (0..$clicks){
-		  $ua->post($url, ['access_token' => $token, 'args' => $code]);
-      usleep($vol_delay);
-    }
-  } else {
-    foreach my $button (@actions{$action}){
-      my $code = $buttons{$button};
-		  $ua->post($url, ['access_token' => $token, 'args' => $code]);
-      usleep($input_delay);
-    }
-  }
+	my ($action, $count) = @_;
+	my $vol_delay = 300;
+	my $input_delay = 1500000;
+	
+	my $ua = LWP::UserAgent->new;
+	my $function = "input";
+	my $url = "https://api.spark.io/v1/devices/$id/$function";
+	
+	my %actions = (
+		"power" => 	["power"],
+		"tv" => 	["tv"],
+		"wii" => 	["vga"],
+		"chromecast" => ["tv", "hdmi"],
+		"laptop" => 	["tv", "hdmi", "hdmi"], 
+		"xbox" => 	["tv", "hdmi", "hdmi", "hdmi"],
+		"volup" => 	["volup"],
+		"voldown" => 	["voldown"],
+		"mute" => 	["mute"],
+	);
+	
+	if ($action =~ /^vol/){
+	my $code = hex $buttons{$action};
+		for my $i (0..$count){
+			$ua->post($url, ['access_token' => $token, 'args' => $code]);
+			usleep($vol_delay);
+		}
+	} else {
+		for my $i (0..$#{ $actions{$action} }){
+			my $button = $actions{$action}[$i];
+			my $code = hex $buttons{$button};
+			$ua->post($url, ['access_token' => $token, 'args' => $code]);
+			usleep($input_delay);
+		}
+	}
 }
 
 
